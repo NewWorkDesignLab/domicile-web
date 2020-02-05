eb5228344dbbab5767ca7651d15c5e609d83c0c1
Tobias Bohn <info@tobiasbohn.com>
2020-02-05 14:15:50 +0100

    [FEATURE] created devise controllers and changed routes

--- app/concepts/domicile/cell/base.rb
+++ app/concepts/domicile/cell/base.rb
@@ -2,9 +2,5 @@ module Domicile::Cell
   class Base < Trailblazer::Cell
     include Domicile::Util::Form
     include Domicile::CookieHelpers
-
-    def default_url_options
-      { lang: I18n.locale }
-    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'pages#index', as: 'index'
+  get 'index.html', to: 'pages#index'
+
+  ## USER ROUTES
+  scope(path_names: {new: I18n.t('routes.misc.new'), edit: I18n.t('routes.misc.edit'), destroy: I18n.t('routes.misc.destroy')}) do
+    devise_for :users,
+        path: I18n.t('routes.misc.users'),
+        controllers: {
+          confirmations: 'users/confirmations',
+          passwords: 'users/passwords',
+          registrations: 'users/registrations',
+          sessions: 'users/sessions',
+          unlocks: 'users/unlocks'
+        },
+        path_names: {
+          sign_in: I18n.t('routes.misc.sign_in'),
+          sign_out: I18n.t('routes.misc.sign_out'),
+          sign_up: I18n.t('routes.misc.sign_up'),
+          password: I18n.t('routes.misc.password'),
+          unlock: I18n.t('routes.misc.unlock'),
+          confirmation: I18n.t('routes.misc.confirmation'),
+          cancel: I18n.t('routes.misc.cancel')
+        }
+  end
 
   get '/scenario', to: 'scenarios#new', as: 'new_scenario'
   post '/scenario', to: 'scenarios#create', as: 'create_scenario'
