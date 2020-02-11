8840bd53f1ff68519d6722aedb0035e389a4a562
Tobias Bohn <info@tobiasbohn.com>
2020-02-11 10:33:55 +0100

    [FEATURE] added customized form builder for select_groups

--- app/concepts/domicile/builder/customized_form_builder.rb
+++ app/concepts/domicile/builder/customized_form_builder.rb
@@ -30,8 +30,10 @@ class Domicile::Builder::CustomizedFormBuilder < ActionView::Helpers::FormBuilde
     text_field_group attribute, options
   end
 
+
+
   def checkbox_group(attribute, options = {})
-    label = label(attribute, options.merge(class: 'form-check-label'))
+    label = label(attribute, options.merge(label_options: { class: 'form-check-label' }))
     hint = help_block(attribute, options)
     error = error_block(attribute, options)
     aria = aria_describedby(attribute, hint, error, options)
@@ -45,13 +47,41 @@ class Domicile::Builder::CustomizedFormBuilder < ActionView::Helpers::FormBuilde
     input = check_box(attribute, opts, checked_value, unchecked_value)
 
     @template.content_tag :div, class: 'form-group form-check' do
-      "#{input}#{label}#{hint}#{error}"
+      "#{input}#{label}#{hint}#{error}".html_safe
     end
   end
 
   def check_box(attribute, options = {}, checked_value, unchecked_value)
     super(attribute, options, checked_value, unchecked_value)
   end
+
+
+
+  def select_group(attribute, choices, options = {})
+    label = label(attribute, options)
+    hint = help_block(attribute, options)
+    error = error_block(attribute, options)
+    aria = aria_describedby(attribute, hint, error, options)
+
+    opts = options[:input_options] || {}
+
+    html_opts = options[:html_options] || {}
+    html_opts = html_opts.merge( required: 'required' ) if options[:required]
+    html_opts = html_opts.merge( aria: { describedby: "#{aria}" } ) unless aria.blank?
+    html_opts = html_opts.merge( class: "form-control#{error_class(attribute)}" )
+
+    input = select(attribute, choices, opts, html_opts)
+
+    @template.content_tag :div, class: 'form-group' do
+      "#{label}#{input}#{hint}#{error}".html_safe
+    end
+  end
+
+  def select(attribute, choices, options = {}, html_options = {})
+    super(attribute, choices, options, html_options)
+  end
+
+
 
   def error_block(attribute, options = {})
     opts = options.delete(:error_options) || {}
@@ -76,10 +106,10 @@ class Domicile::Builder::CustomizedFormBuilder < ActionView::Helpers::FormBuilde
   end
 
   def label(attribute, options = {})
+    opts = options.delete(:label_options) || {}
     required = @template.content_tag :span, '&nbsp;*'.html_safe, aria: { hidden: 'true' } if options[:label] && options[:required]
-    text = options.delete(:label) if options[:label]
-
-    super(attribute, "#{text}#{required}".html_safe, options.delete(:label_options) || {})
+    text = options[:label] if options[:label]
+    super(attribute, "#{text}#{required}".html_safe, opts) if options.delete(:label)
   end
 
   def aria_describedby(attribute, hint, error, options = {})
