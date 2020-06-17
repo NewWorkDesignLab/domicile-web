77bd44dd8427e09bcec9b471d2d9f59fdfbbc11a
Tobias Bohn <info@tobiasbohn.com>
2020-06-17 04:26:27 +0200

    [FEATURE] further spectator and websocket implementation

--- app/channels/spectator_channel.rb
+++ app/channels/spectator_channel.rb
@@ -1,9 +1,18 @@
 class SpectatorChannel < ApplicationCable::Channel
   def subscribed
+    check_participation_accessibility
     stream_from "spectate_participation_#{params[:id]}"
   end
 
-  def receive(data)
+  def update_transform(data)
     ActionCable.server.broadcast("spectate_participation_#{params[:id]}", data)
+  end
+
+  private
+
+  def check_participation_accessibility
+    unless current_user&.available_participations.exists?(id: params[:id])
+      reject_unauthorized_connection
+    end
   end
 end
--- app/javascript/controllers/spectator_controller.js
+++ app/javascript/controllers/spectator_controller.js
@@ -3,12 +3,13 @@ import consumer from "../channels/consumer"
 
 export default class extends Controller {
   initialize() {
+    var that = this;
     this.socket = consumer.subscriptions.create({ channel: "SpectatorChannel", id: this.data.get("id") }, {
       connected() {
-        this.unityInstance = UnityLoader.instantiate("webgl-target", "/unityweb/Build/webgl_test_002.json", { onProgress: UnityProgress });
+        that.unityInstance = UnityLoader.instantiate("webgl-target", "/unityweb/Build/webgl_test_006.json", { onProgress: UnityProgress });
       },
       received(data) {
-        this.sendDataToWebGL(data);
+        that.sendDataToWebGL(data["parameter"]);
       }
     });
   }
@@ -20,6 +21,8 @@ export default class extends Controller {
   sendDataToWebGL(data) {
     // document.getElementsByClassName("webgl-content")[0].spectator.sendDataToWebGL()
     // Send the received Data from VR-Player inside all Unity-WebGL-Apps of Channel Subscriptions (Spectators)
-    this.unityInstance.SendMessage("BrowserCommunicator", "Message", data);
+    var jsonData = JSON.parse(data);
+    console.log("Sednding to WebGL: " + JSON.stringify(jsonData));
+    this.unityInstance.SendMessage("BrowserCommunicator", "Message", JSON.stringify(jsonData));
   }
 }
