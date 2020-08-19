b243d469425f92ca0e61831caf854a3cf178c0a9
Tobias Bohn <info@tobiasbohn.com>
2020-08-19 17:47:24 +0200

    [FEATURE] added spectate button and updated webgl spectator app

--- app/javascript/controllers/spectator_controller.js
+++ app/javascript/controllers/spectator_controller.js
@@ -6,7 +6,7 @@ export default class extends Controller {
     var that = this;
     this.socket = consumer.subscriptions.create({ channel: "SpectatorChannel", id: this.data.get("id") }, {
       connected() {
-        that.unityInstance = UnityLoader.instantiate("webgl-target", "/unityweb/Build/webgl_test_006.json", { onProgress: UnityProgress });
+        that.unityInstance = UnityLoader.instantiate("webgl-target", "/unityweb/Build/webgl_test_007.json", { onProgress: UnityProgress });
       },
       received(data) {
         that.sendDataToWebGL(data["parameter"]);
